ed30aefc1429f002dfa704e6e39f6b98f962959f
Marat <ashrapovm@yandex.ru>
2023-03-02 19:52:01 +0400

    Добавлено метод для обработка WAV файл

--- Client/PSASH.Infrastructure/Services/FileBased/Converter/FileBasedMonoTimeSeriesConverter.cs
+++ Client/PSASH.Infrastructure/Services/FileBased/Converter/FileBasedMonoTimeSeriesConverter.cs
@@ -2,8 +2,10 @@
 using PSASH.Core.Entities;
 using PSASH.Core.ValueObjects;
 using PSASH.Infrastructure.Exceptions;
+using System.Data;
 using System.Globalization;
 using System.Text.RegularExpressions;
+using NAudio;
 
 namespace PSASH.Infrastructure.Services.FileBased.Converter
 {
@@ -18,10 +20,47 @@ namespace PSASH.Infrastructure.Services.FileBased.Converter
         /// <exception cref="NotImplementedException"></exception>
         public MonoTimeSeries Convert(string input)
         {
-            List<double> valuesCSV = new List<double>();
-            var delimiter = ";";
+            string ext = Path.GetExtension(input);
+            if (ext == ".csv") {
+                return CSVProcessig(input);
+            }
+            if (ext == ".wav")
+            {
+                return WAVProcessing(input);
+            }
+            else
+            {
+                throw new FileExtensionNotSupportedException();
+            }
+            
+        }
+
+        private MonoTimeSeries WAVProcessing(string input)
+        {
+            MonoTimeSeries mono;
+            using (NAudio.Wave.WaveFileReader wave = new NAudio.Wave.WaveFileReader(input))
+            {
+                List<double> valuesWAV = new List<double>();
+                byte[] data = new byte[wave.Length];
+                int read = wave.Read(data, 0, data.Length);
+                for (int i = 0; i < read; i += 2)
+                {
+                    valuesWAV.Add(BitConverter.ToInt16(data, i) / (wave.WaveFormat.SampleRate * 2));
+                }
+                string[] parserPath = input.Split(new char[] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
+                TimeSeriesInfo info = new TimeSeriesInfo(parserPath[parserPath.Length - 1], parserPath[parserPath.Length]);
+                mono = new MonoTimeSeries(valuesWAV, info);
+            }
+            return mono;
+        }
+
+        private MonoTimeSeries CSVProcessig(string input)
+        {
+            MonoTimeSeries mono;
             using (var parser = new TextFieldParser(input))
             {
+                List<double> valuesCSV = new List<double>();
+                var delimiter = ";";
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(delimiter);
                 while (!parser.EndOfData)
@@ -41,13 +80,12 @@ namespace PSASH.Infrastructure.Services.FileBased.Converter
 
                     }
                 }
-                
-                string[] parserPath = input.Split(new char[] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries); 
-                TimeSeriesInfo info = new TimeSeriesInfo(parserPath[parserPath.Length-1], parserPath[parserPath.Length]);
-                var mono = new MonoTimeSeries(valuesCSV, info);
-                return mono;
 
+                string[] parserPath = input.Split(new char[] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
+                TimeSeriesInfo info = new TimeSeriesInfo(parserPath[parserPath.Length - 1], parserPath[parserPath.Length]);
+                mono = new MonoTimeSeries(valuesCSV, info);
             }
+            return mono;
         }
 
         private enum AvailableExtensions
