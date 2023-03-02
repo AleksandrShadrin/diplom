0f15433472879a218624d5ae8d512c9e4b413ae2
Marat <ashrapovm@yandex.ru>
2023-03-02 19:57:21 +0400

    ver 2

--- Client/PSASH.Infrastructure/Exceptions/FileExtensionNotSupportedException.cs
+++ Client/PSASH.Infrastructure/Exceptions/FileExtensionNotSupportedException.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace PSASH.Infrastructure.Exceptions
 {
-    internal class FileExtensionNotSupportedException
+    internal class FileExtensionNotSupportedException : InfrastructureException
     {
+        public FileExtensionNotSupportedException()
+            : base("Расширение файла не поддерживается")
+        {
+        }
     }
 }
