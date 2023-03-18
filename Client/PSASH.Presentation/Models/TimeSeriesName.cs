7ec61be09a61f0ca3129d8642a9e230b172a8b97
Alex <ssaasshha11@yandex.ru>
2023-03-18 15:41:58 +0400

    upd

---
+++ Client/PSASH.Presentation/Models/TimeSeriesName.cs
@@ -0,0 +1,26 @@
+﻿using PSASH.Core.ValueObjects;
+
+namespace PSASH.Presentation.Models
+{
+    public class TimeSeriesName
+    {
+        public string Value { get; set; }
+
+        private TimeSeriesName(string value)
+        {
+            Value = value;
+        }
+
+        public static TimeSeriesName FromTimeSeriesInfo(TimeSeriesInfo timeSeriesInfo)
+            => new($"{timeSeriesInfo.Class}:{timeSeriesInfo.id}");
+
+        public TimeSeriesInfo ToTimeSeriesInfo()
+        {
+            var parts = Value.Split(':');
+            return new TimeSeriesInfo(parts[0], parts[1]);
+        }
+
+        public override string ToString()
+            => Value;
+    }
+}
