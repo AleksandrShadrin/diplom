f71e5394475b7f8ff4b475be27b522a0cef5ae2e
Marat <ashrapovm@yandex.ru>
2023-03-19 17:24:48 +0400

    Добавлены методы для вычисления характеристик временного ряда

---
+++ Client/PSASH.Presentation/Services/ITimeSeriesInfoService.cs
@@ -0,0 +1,14 @@
+﻿using PSASH.Presentation.Models;
+
+namespace PSASH.Presentation.Services
+{
+    public interface ITimeSeriesInfoService
+    {
+        double GetMean(TimeSeriesDto timeSeries);
+        double GetStd(TimeSeriesDto timeSeries);
+
+        double GetCV(TimeSeriesDto timeSeries);
+        double GetMax(TimeSeriesDto timeSeries);
+        double GetMin(TimeSeriesDto timeSeries);
+    }
+}
---
+++ Client/PSASH.Presentation/Services/TimeSeriesInfoService.cs
@@ -0,0 +1,65 @@
+﻿using PSASH.Presentation.Models;
+
+
+namespace PSASH.Presentation.Services
+{
+    public class TimeSeriesInfoService : ITimeSeriesInfoService
+    {
+        public double GetMean(TimeSeriesDto timeSeries)
+        {
+            var values = timeSeries.Values;
+            double sum = 0;
+            foreach (var value in values)
+            {
+                sum += value;
+            }
+            return sum / values.Count;
+        }
+        public double GetStd(TimeSeriesDto timeSeries)
+        {
+            var values = timeSeries.Values;
+            double sum = 0;
+            foreach (var value in values)
+            {
+                sum += value;
+            }
+            double mean = sum / values.Count;
+            double sumStd = 0;
+            foreach (var value in values)
+            {
+                sumStd += Math.Pow((value - mean), 2);
+            }
+            return Math.Sqrt(sumStd / values.Count);
+        }
+
+        public double GetCV(TimeSeriesDto timeSeries)//коэффициент вариации
+        {
+            var values = timeSeries.Values;
+            double sum = 0;
+            foreach (var value in values)
+            {
+                sum += value;
+            }
+            double mean = sum / values.Count;
+            double sum2 = 0;
+            foreach (var value in values)
+            {
+                sum2 += Math.Pow((value - mean), 2) / (values.Count-1);
+            }
+            double std = Math.Sqrt(sum2);
+            return std*100/mean;
+        }
+
+        public double GetMax(TimeSeriesDto timeSeries)
+        {
+            var values = timeSeries.Values;
+            return values.Max();
+        }
+
+        public double GetMin(TimeSeriesDto timeSeries)
+        {
+            var values = timeSeries.Values;
+            return values.Min();
+        }
+    }
+}
