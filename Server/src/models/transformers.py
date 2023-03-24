b51adf5c475a23278a9cb05983d18a1cf25a0ff1
Alex <ssaasshha11@yandex.ru>
2023-03-24 18:14:22 +0400

    upd

--- Server/src/models/transformers.py
+++ Server/src/models/transformers.py
@@ -1,8 +1,13 @@
 from models.TimeSeries import TimeSeries
+from models.Dataset import Dataset
 
 
 class BaseTransformer:
     """Base TimeSeries transformer"""
+
+    def train(self, dataset: Dataset):
+        """Train transformer using dataset"""
+        raise NotImplementedError("BaseTransformer don't imlement this method")
 
     def transform(self, time_series: TimeSeries):
         """Transform TimeSeries"""
